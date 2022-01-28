b20d461f01bf16a76df87a52382b1c4c7d41d3f0
Maxim Vainikka <maxim.vainikka@yandex.ru>
2022-01-28 20:45:33 +0300

    Initial Commit

---
+++ Dice/DiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceApp.swift
+//  Dice
+//
+//  Created by Максим Нуждин on 28.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct DiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
